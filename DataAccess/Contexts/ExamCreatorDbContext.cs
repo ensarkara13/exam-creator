dd7c386c5c208add90f153a4f1f2f5f4e28bf9b2
Ensar Karamürteza <ensarkara13@gmail.com>
2022-06-02 17:47:55 +0300

    ExamCreatorDbContext created

---
+++ DataAccess/Contexts/ExamCreatorDbContext.cs
@@ -0,0 +1,57 @@
+using Core.Entities;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace DataAccess.Contexts
+{
+  public class ExamCreatorDbContext : IdentityDbContext
+  {
+    public ExamCreatorDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Exam> Exams { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<QuestionOption> QuestionOptions { get; set; }
+    public DbSet<UserExam> UserExams { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<User>()
+      .HasMany(u => u.UserExams)
+      .WithOne(u => u.User);
+
+      builder.Entity<Exam>()
+      .HasMany(e => e.UserExams)
+      .WithOne(e => e.Exam)
+      .OnDelete(DeleteBehavior.Cascade);
+
+      builder.Entity<Exam>()
+      .HasMany(e => e.Questions)
+      .WithOne(e => e.Exam)
+      .OnDelete(DeleteBehavior.Cascade);
+
+      builder.Entity<Question>()
+      .HasMany(q => q.QuestionOptions)
+      .WithOne(q => q.Question)
+      .OnDelete(DeleteBehavior.Cascade);
+    }
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+      IEnumerable<EntityEntry<EntityBase>> entries = ChangeTracker.Entries<EntityBase>();
+
+      foreach (EntityEntry<EntityBase> entry in entries)
+      {
+        switch (entry.State)
+        {
+          case EntityState.Added:
+            entry.Entity.CreatedAt = DateTime.Now;
+            break;
+        }
+      }
+
+      return await base.SaveChangesAsync(cancellationToken);
+    }
+  }
+}
