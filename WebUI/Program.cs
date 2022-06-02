b4d310ff45c58656a6b37c3d13e788efdd9c3df3
Ensar Karamürteza <ensarkara13@gmail.com>
2022-06-02 18:00:22 +0300

    DataAccess dependencies injected

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,4 +1,9 @@
+using DataAccess;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = builder.Configuration;
+
+builder.Services.AddDataAccess(configuration);
 
 builder.Services.AddControllersWithViews();
 
