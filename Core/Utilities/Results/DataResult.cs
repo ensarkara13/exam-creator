69b70708f6dad1b8e7e91a0fede7ad270b67e9e9
Ensar Karamürteza <ensarkara13@gmail.com>
2022-06-03 09:30:54 +0300

    result classes added

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,51 @@
+using Core.Models;
+using Core.Extensions;
+using FluentValidation.Results;
+
+namespace Core.Utilities.Results
+{
+  public class DataResult<T>
+  {
+    public bool IsSuccess { get; private set; }
+    public string Message { get; private set; }
+    public List<CustomValidationError> ErrorMessages { get; private set; }
+    public T Data { get; private set; }
+
+    public static DataResult<T> Success(T data)
+    {
+      return new DataResult<T>()
+      {
+        IsSuccess = true,
+        Data = data
+      };
+    }
+
+    public static DataResult<T> Success(T data, string message)
+    {
+      return new DataResult<T>()
+      {
+        IsSuccess = true,
+        Data = data,
+        Message = message
+      };
+    }
+
+    public static DataResult<T> Failure(string message)
+    {
+      return new DataResult<T>()
+      {
+        IsSuccess = false,
+        Message = message
+      };
+    }
+
+    public static DataResult<T> Failure(ValidationResult validationResult)
+    {
+      return new DataResult<T>()
+      {
+        IsSuccess = false,
+        ErrorMessages = validationResult.ParseToCustomErrors()
+      };
+    }
+  }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,48 @@
+using Core.Models;
+using Core.Extensions;
+using FluentValidation.Results;
+
+namespace Core.Utilities.Results
+{
+  public class Result
+  {
+    public bool IsSuccess { get; private set; }
+    public string Message { get; private set; }
+    public List<CustomValidationError> ErrorMessages { get; private set; }
+
+    public static Result Success()
+    {
+      return new Result()
+      {
+        IsSuccess = true
+      };
+    }
+
+    public static Result Success(string message)
+    {
+      return new Result()
+      {
+        IsSuccess = true,
+        Message = message
+      };
+    }
+
+    public static Result Failure(string message)
+    {
+      return new Result()
+      {
+        IsSuccess = false,
+        Message = message
+      };
+    }
+
+    public static Result Failure(ValidationResult validationResult)
+    {
+      return new Result()
+      {
+        IsSuccess = false,
+        ErrorMessages = validationResult.ParseToCustomErrors()
+      };
+    }
+  }
+}
