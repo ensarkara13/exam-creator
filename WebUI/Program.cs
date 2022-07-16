d7123a010efd1134cf3274ebee0a8a0c1cc58b3a
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-16 18:27:35 +0300

    Removed usings and dependency injections

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,11 +1,5 @@
-using Business;
-using DataAccess;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 IConfiguration configuration = builder.Configuration;
-
-builder.Services.AddDataAccess(configuration);
-builder.Services.AddBusinessLogic();
 
 builder.Services.AddControllersWithViews();
 
