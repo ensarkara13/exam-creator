8ce823eb4d1624a0bf6072f4bd854ad95f58bbd1
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-16 16:18:49 +0300

    Created file

---
+++ Business/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Core.Utilities.Results;
+global using Entities.Concrete;
+global using Entities.DTOs.Exam;
+global using Entities.DTOs.Question;
+global using Entities.DTOs.QuestionOption;
+global using AutoMapper;
+global using FluentValidation;
+global using FluentValidation.Results;
