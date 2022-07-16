501deee9b6bf5242374d13555ee8b5664ca335d4
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-16 13:46:27 +0300

    Added id

--- Entities/DTOs/QuestionOption/QuestionOptionGetDto.cs
+++ Entities/DTOs/QuestionOption/QuestionOptionGetDto.cs
@@ -2,6 +2,7 @@ namespace Entities.DTOs.QuestionOption
 {
   public class QuestionOptionGetDto
   {
+    public int Id { get; set; }
     public string OptionText { get; set; }
     public bool IsRightAnswer { get; set; }
   }
