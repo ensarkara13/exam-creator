97880414109278d0fcf65b030e1d9468aaad3f6d
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-16 18:28:52 +0300

    Added global usings

--- Business/GlobalUsings.cs
+++ Business/GlobalUsings.cs
@@ -3,6 +3,10 @@ global using Entities.Concrete;
 global using Entities.DTOs.Exam;
 global using Entities.DTOs.Question;
 global using Entities.DTOs.QuestionOption;
+global using DataAccess.Repositories.Abstract;
+global using Business.Abstract;
+
+
 global using AutoMapper;
 global using FluentValidation;
 global using FluentValidation.Results;
