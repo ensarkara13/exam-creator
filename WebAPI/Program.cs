d8499f9128e705ad5d27283d57b82daf429e3b53
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-16 22:37:46 +0300

    Added dependencies to the container

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,25 @@
+using Business;
+using DataAccess;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBusinessLogic();
+builder.Services.AddDataAccess(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+
 WebApplication app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(opt =>
+{
+  opt.AllowAnyHeader();
+  opt.AllowAnyMethod();
+  opt.AllowAnyOrigin();
+});
+
+app.UseRouting();
+
+app.UseEndpoints(endpoint => endpoint.MapControllers());
 
 app.Run();
