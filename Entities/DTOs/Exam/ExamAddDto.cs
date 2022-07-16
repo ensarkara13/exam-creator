c9bcb11815566b269cf524249e9464b717666ec0
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-16 22:41:25 +0300

    Added properties

--- Entities/DTOs/Exam/ExamAddDto.cs
+++ Entities/DTOs/Exam/ExamAddDto.cs
@@ -1,7 +1,10 @@
+using Entities.DTOs.Question;
+
 namespace Entities.DTOs.Exam
 {
   public class ExamAddDto
   {
     public string Title { get; set; }
+    public List<QuestionAddDto> Questions { get; set; }
   }
 }
--- Entities/DTOs/Question/QuestionAddDto.cs
+++ Entities/DTOs/Question/QuestionAddDto.cs
@@ -1,8 +1,11 @@
+using Entities.DTOs.QuestionOption;
+
 namespace Entities.DTOs.Question
 {
   public class QuestionAddDto
   {
     public string QuestionText { get; set; }
     public int ExamId { get; set; }
+    public List<QuestionOptionAddDto> QuestionOptions { get; set; }
   }
 }
