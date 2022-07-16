352e400c37de399b9f4668ed8fe08a50edd0570c
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-16 16:21:09 +0300

    Changed file

--- Core/Entities/EntityBase.cs
+++ Core/Entities/EntityBase.cs
@@ -1,11 +1,10 @@
-using System;
 
 namespace Core.Entities
 {
   public abstract class EntityBase
   {
     public int Id { get; set; }
-    public DateTime CreatedAt { get; set; }
     public bool Status { get; set; }
+    public DateTime CreatedAt { get; set; }
   }
 }
