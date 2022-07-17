8b6f5886854daafb4e1f45a2ef3292366756a8f0
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-17 14:56:48 +0300

    Changed parameter id type to Guid

--- Business/Abstract/IExamService.cs
+++ Business/Abstract/IExamService.cs
@@ -4,9 +4,9 @@ namespace Business.Abstract
   public interface IExamService
   {
     Task<Result> AddExamAsync(ExamAddDto examAddDto);
-    Task<Result> DeleteExamAsync(int id);
+    Task<Result> DeleteExamAsync(Guid id);
     Task<Result> UpdateExamAsync(ExamUpdateDto examUpdateDto);
     Task<DataResult<List<ExamGetDto>>> GetExamListAsync();
-    Task<DataResult<ExamGetWithQuestionsDto>> GetExamAsync(int id);
+    Task<DataResult<ExamGetWithQuestionsDto>> GetExamAsync(Guid id);
   }
 }
