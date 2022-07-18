60c909786f051520d4e896b0bb6813d2eb6f1887
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-18 16:52:51 +0300

    Added validation to identity

--- DataAccess/DependencyInjection.cs
+++ DataAccess/DependencyInjection.cs
@@ -20,6 +20,8 @@ namespace DataAccess
         options.Password.RequireDigit = true;
         options.Password.RequireLowercase = true;
         options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredUniqueChars = 0;
 
         options.User.RequireUniqueEmail = true;
       }).AddEntityFrameworkStores<ExamCreatorDbContext>();
