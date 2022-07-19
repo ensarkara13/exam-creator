1a931a8171a12b6388fe2996c8cac05b7ef7f42b
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 22:14:41 +0300

    Created ViewModels

---
+++ WebUI/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebUI.Models.ViewModels
+{
+  public class LoginViewModel
+  {
+    public string Email { get; set; }
+    public string Password { get; set; }
+  }
+}
---
+++ WebUI/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+namespace WebUI.Models.ViewModels
+{
+  public class RegisterViewModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PasswordConfirm { get; set; }
+  }
+}
