5e58c25d9ce89f33d65404b692b2f4c351b91df0
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 15:40:09 +0300

    Added User Dto using

--- Business/GlobalUsings.cs
+++ Business/GlobalUsings.cs
@@ -3,9 +3,9 @@ global using Entities.Concrete;
 global using Entities.DTOs.Exam;
 global using Entities.DTOs.Question;
 global using Entities.DTOs.QuestionOption;
+global using Entities.DTOs.User;
 global using DataAccess.Repositories.Abstract;
 global using Business.Abstract;
-
 
 global using AutoMapper;
 global using FluentValidation;
