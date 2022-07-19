3a659d177e25cb598781078b22c4dbeb9e9e145c
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 15:41:15 +0300

    Created manager

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Business.Concrete
+{
+  public class UserManager : IUserService
+  {
+    private readonly IMapper _mapper;
+    public UserManager()
+    {
+    }
+
+    public Task<Result> AddUserAsync(UserAddDto userAddDto)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<DataResult<UserGetDto>> GetUserByEmailAsync(string email)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
