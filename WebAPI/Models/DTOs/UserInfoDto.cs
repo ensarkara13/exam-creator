e53083ef6164a8259386de580b8b72b468f7ced3
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 17:04:23 +0300

    Created Dto for user info

---
+++ WebAPI/Models/DTOs/UserInfoDto.cs
@@ -0,0 +1,12 @@
+
+namespace WebAPI.Models.DTOs
+{
+  public class UserInfoDto
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public string AccessToken { get; set; }
+  }
+}
