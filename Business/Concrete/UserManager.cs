095927eae74f7deb9ce08589f54185dc504c354f
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 16:06:14 +0300

    Added dependencies via constructor

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,11 +1,20 @@
 
+using Microsoft.AspNetCore.Identity;
+
 namespace Business.Concrete
 {
   public class UserManager : IUserService
   {
+    private readonly IUserRepository _repository;
     private readonly IMapper _mapper;
-    public UserManager()
+    private readonly IValidator<UserAddDto> _addValidator;
+    private readonly IPasswordHasher<string> _passwordHasher;
+    public UserManager(IUserRepository repository, IMapper mapper, IValidator<UserAddDto> addValidator, IPasswordHasher<string> passwordHasher)
     {
+      _repository = repository;
+      _mapper = mapper;
+      _addValidator = addValidator;
+      _passwordHasher = passwordHasher;
     }
 
     public Task<Result> AddUserAsync(UserAddDto userAddDto)
