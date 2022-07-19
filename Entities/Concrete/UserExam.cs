2821f9b98bab74b13deb655aa0528e244a99685c
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 17:31:43 +0300

    Changed User Id type to Guid

--- Entities/Concrete/UserExam.cs
+++ Entities/Concrete/UserExam.cs
@@ -4,7 +4,7 @@ namespace Entities.Concrete
   {
     public int Score { get; set; }
 
-    public int UserId { get; set; }
+    public Guid UserId { get; set; }
     public User User { get; set; }
     public Guid ExamId { get; set; }
     public Exam Exam { get; set; }
