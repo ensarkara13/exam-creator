3d86564108409550080f625746f4a9ebfd3e8a9a
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 15:28:53 +0300

    Removed identity dependency

--- DataAccess/DependencyInjection.cs
+++ DataAccess/DependencyInjection.cs
@@ -1,7 +1,6 @@
 using DataAccess.Contexts;
 using DataAccess.Repositories.Abstract;
 using DataAccess.Repositories.Concrete.EntityFramework;
-using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,18 +12,6 @@ namespace DataAccess
     public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
     {
       services.AddDbContext<ExamCreatorDbContext>(options => options.UseSqlite(configuration.GetConnectionString("ExamCreatorDB")));
-
-      services.AddIdentity<User, Role>(options =>
-      {
-        options.Password.RequiredLength = 8;
-        options.Password.RequireDigit = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireUppercase = true;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequiredUniqueChars = 0;
-
-        options.User.RequireUniqueEmail = true;
-      }).AddEntityFrameworkStores<ExamCreatorDbContext>();
 
       services.AddScoped<IExamRepository, EFExamRepository>();
       services.AddScoped<IUserExamRepository, EFUserExamRepository>();
