a612e27ac9780592673c54c376ce2eb547acc465
Ensar Karamürteza <ensarkara13@gmail.com>
2022-07-19 15:34:42 +0300

    Changed properties and base class

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -1,11 +1,14 @@
-using Microsoft.AspNetCore.Identity;
+using Core.Entities;
 
 namespace Entities.Concrete
 {
-  public class User : IdentityUser<int>
+  public class User : EntityBase
   {
-    public string Name { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
 
     public ICollection<UserExam> UserExams { get; set; }
   }
